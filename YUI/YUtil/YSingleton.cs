6bf4116f4edb15047fd70a4b14ad43324e5a7203
yjk282 <1901228899@qq.com>
2017-05-31 13:36:03 +0800

    基础结构

---
+++ YUI/YUtil/YSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YUI.YUtil
+{
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    /// <typeparam name="TItem"></typeparam>
+    public static class YSingleton<TItem> where TItem : class, new()
+    {
+        private static TItem _instance;
+
+        /// <summary>
+        /// 获取单例模式
+        /// </summary>
+        /// <returns></returns>
+        public static TItem GetInstance()
+        {
+            if (_instance == null)
+                Interlocked.CompareExchange(ref _instance, Activator.CreateInstance<TItem>(), default(TItem));
+
+            return _instance;
+        }
+    }
+}
