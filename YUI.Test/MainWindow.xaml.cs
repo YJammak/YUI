40f38427f9476c04a82b8f29ef564f041c5270ff
yjk282 <1901228899@qq.com>
2017-06-03 15:50:55 +0800

    添加YDataGrid

--- YUI.Test/MainWindow.xaml.cs
+++ YUI.Test/MainWindow.xaml.cs
@@ -15,14 +15,33 @@ using System.Windows.Shapes;
 
 namespace YUI.Test
 {
+    public class DataGridTest
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<DataGridTest> Data { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Data = new List<DataGridTest>();
+
+            Data.Add(new DataGridTest {ID = "1", Name = "Smith", Phone = "1234125412", Address = "asdgbhiuas"});
+            Data.Add(new DataGridTest {ID = "2", Name = "Jion", Phone = "1234125412", Address = "asdgbhiuas"});
+            Data.Add(new DataGridTest {ID = "3", Name = "asdf", Phone = "1234125412", Address = "asdgbhiuas"});
+            Data.Add(new DataGridTest {ID = "4", Name = "asdgas", Phone = "1234125412", Address = "asdgbhiuas"});
+            Data.Add(new DataGridTest {ID = "5", Name = "dsg", Phone = "1234125412", Address = "asdgbhiuas"});
+            Data.Add(new DataGridTest {ID = "6", Name = "asdg", Phone = "1234125412", Address = "asdgbhiuas"});
         }
     }
 }
