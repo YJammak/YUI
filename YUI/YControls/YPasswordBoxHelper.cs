1baff131ae49141cb322642b5b3bc070fa2d4ae2
yjk <1901228899@qq.com>
2017-06-03 22:14:24 +0800

    添加YPasswordBox

---
+++ YUI/YControls/YPasswordBoxHelper.cs
@@ -0,0 +1,141 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace YUI.YControls
+{
+    /// <summary>   
+    /// 为PasswordBox控件的Password增加绑定功能   
+    /// </summary>   
+    public static class YPasswordBoxHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.RegisterAttached("Password",
+            typeof(string), typeof(YPasswordBoxHelper),
+            new FrameworkPropertyMetadata(string.Empty, OnPasswordPropertyChanged));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty AttachProperty =
+            DependencyProperty.RegisterAttached("Attach",
+            typeof(bool), typeof(YPasswordBoxHelper), new PropertyMetadata(false, Attach));
+
+        private static readonly DependencyProperty IsUpdatingProperty =
+           DependencyProperty.RegisterAttached("IsUpdating", typeof(bool),
+           typeof(YPasswordBoxHelper));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty PasswordLengthProperty =
+            DependencyProperty.RegisterAttached("PasswordLength", typeof(int), typeof(YPasswordBoxHelper), new UIPropertyMetadata(0));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static int GetPasswordLength(DependencyObject obj)
+        {
+            return (int)obj.GetValue(PasswordLengthProperty);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetPasswordLength(DependencyObject obj, int value)
+        {
+            obj.SetValue(PasswordLengthProperty, value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dp"></param>
+        /// <param name="value"></param>
+        public static void SetAttach(DependencyObject dp, bool value)
+        {
+            dp.SetValue(AttachProperty, value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dp"></param>
+        /// <returns></returns>
+        public static bool GetAttach(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(AttachProperty);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dp"></param>
+        /// <returns></returns>
+        public static string GetPassword(DependencyObject dp)
+        {
+            return (string)dp.GetValue(PasswordProperty);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dp"></param>
+        /// <param name="value"></param>
+        public static void SetPassword(DependencyObject dp, string value)
+        {
+            dp.SetValue(PasswordProperty, value);
+        }
+        private static bool GetIsUpdating(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(IsUpdatingProperty);
+        }
+        private static void SetIsUpdating(DependencyObject dp, bool value)
+        {
+            dp.SetValue(IsUpdatingProperty, value);
+        }
+
+        private static void OnPasswordPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var passwordBox = sender as PasswordBox;
+            if (passwordBox == null) return;
+            
+            passwordBox.PasswordChanged -= PasswordChanged;
+            if (!GetIsUpdating(passwordBox))
+            {
+                passwordBox.Password = (string)e.NewValue;
+            }
+            passwordBox.PasswordChanged += PasswordChanged;
+        }
+        private static void Attach(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var passwordBox = sender as PasswordBox;
+            if (passwordBox == null) return;
+
+            if ((bool)e.OldValue)
+            {
+                passwordBox.PasswordChanged -= PasswordChanged;
+            }
+            if ((bool)e.NewValue)
+            {
+                passwordBox.PasswordChanged += PasswordChanged;
+            }
+        }
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var passwordBox = sender as PasswordBox;
+            if (passwordBox == null) return;
+
+            SetIsUpdating(passwordBox, true);
+            SetPassword(passwordBox, passwordBox.Password);
+            SetPasswordLength(passwordBox, passwordBox.Password.Length);
+            SetIsUpdating(passwordBox, false);
+        }
+    }
+}
