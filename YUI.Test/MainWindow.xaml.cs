669b44ed6735a814550428f4b4041b3d13498f77
yjk <1901228899@qq.com>
2017-06-04 02:53:42 +0800

    添加YListBox

--- YUI.Test/MainWindow.xaml.cs
+++ YUI.Test/MainWindow.xaml.cs
@@ -22,6 +22,11 @@ namespace YUI.Test
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 
     /// <summary>
