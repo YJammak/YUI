3234212c2bee9863be44321a4e82b4e53c3b441c
yjk282 <1901228899@qq.com>
2017-09-07 12:30:12 +0800

    添加程序单例模式类YSingleInstanceManager，YMessageBox添加默认按钮功能

---
+++ YUI.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+using YUI.YUtil;
+
+namespace YUI.Test
+{
+    static class Program
+    {
+        [System.STAThread()]
+        [System.Diagnostics.DebuggerNonUserCode()]
+        [System.CodeDom.Compiler.GeneratedCode("PresentationBuildTasks", "4.0.0.0")]
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            var manager = new YSingleInstanceManager(app);
+            manager.NextInstanceStartuped += line =>
+            {
+                Application.Current.MainWindow?.Activate();
+            };
+            manager.Run(args);
+        }
+    }
+}
---
+++ YUI/YUtil/YSingleInstanceManager.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace YUI.YUtil
+{
+    /// <summary>
+    /// 下一个实例启动事件句柄
+    /// </summary>
+    /// <param name="commandLine"></param>
+    public delegate void NextInstanceStartupedHandle(IReadOnlyCollection<string> commandLine);
+
+    /// <summary>
+    /// 程序单例模式
+    /// </summary>
+    public class YSingleInstanceManager
+    {
+        /// <summary>
+        /// 下一个实例启动事件
+        /// </summary>
+        public event NextInstanceStartupedHandle NextInstanceStartuped;
+
+        private SingleInstanceManager manager { get; set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="app"></param>
+        public YSingleInstanceManager(Application app)
+        {
+            manager = new SingleInstanceManager(app);
+            manager.NextInstanceStartuped += args =>
+            {
+                NextInstanceStartuped?.Invoke(args);
+            };
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="commadLine"></param>
+        public void Run(string[] commadLine)
+        {
+            manager?.Run(commadLine);
+        }
+    }
+
+
+    internal class SingleInstanceManager : WindowsFormsApplicationBase
+    {
+
+        /// <summary>
+        /// 下一个实例启动事件
+        /// </summary>
+        public event NextInstanceStartupedHandle NextInstanceStartuped;
+
+        private Application App { get; set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="app"></param>
+        public SingleInstanceManager(Application app)
+        {
+            IsSingleInstance = true;
+            App = app ?? throw new ArgumentNullException(nameof(app));
+        }
+
+        /// <summary>
+        /// 程序启动
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        protected override bool OnStartup(Microsoft.VisualBasic.ApplicationServices.StartupEventArgs e)
+        {
+            // First time app is launched
+            App.Run();
+            return false;
+        }
+
+        /// <summary>
+        /// 第二个实例启动时处理函数
+        /// </summary>
+        /// <param name="eventArgs"></param>
+        protected override void OnStartupNextInstance(StartupNextInstanceEventArgs eventArgs)
+        {
+            // Subsequent launches
+            base.OnStartupNextInstance(eventArgs);
+            NextInstanceStartuped?.Invoke(eventArgs.CommandLine);
+        }
+    }
+}
