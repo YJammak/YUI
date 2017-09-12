1d573f2b47f810a530e40505f7724e65f217b2f0
yjk282 <1901228899@qq.com>
2017-09-12 17:55:57 +0800

    添加快捷方式辅助类YShortcutHelper, YJsonHelper添加SerializeObjectToFormatJson

---
+++ YUI/YUtil/YShortcutHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IWshRuntimeLibrary;
+
+namespace YUI.YUtil
+{
+    /// <summary>
+    /// 快捷方式辅助类
+    /// </summary>
+    public static class YShortcutHelper
+    {
+        /// <summary>
+        /// 创建快捷方式
+        /// </summary>
+        /// <param name="directory">快捷方式所处的文件夹</param>
+        /// <param name="shortcutName">快捷方式名称</param>
+        /// <param name="targetPath">目标路径</param>
+        /// <param name="description">描述</param>
+        /// <param name="iconLocation">图标路径，格式为"可执行文件或DLL路径, 图标编号"，
+        /// 例如System.Environment.SystemDirectory + "\\" + "shell32.dll, 165"</param>
+        /// <remarks></remarks>
+        public static void CreateShortcut(string directory, string shortcutName, string targetPath,
+            string description = null, string iconLocation = null)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            var shortcutPath = Path.Combine(directory, $"{shortcutName}.lnk");
+            var shell = new WshShell();
+            var shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);//创建快捷方式对象
+            shortcut.TargetPath = targetPath;//指定目标路径
+            shortcut.WorkingDirectory = Path.GetDirectoryName(targetPath);//设置起始位置
+            shortcut.WindowStyle = 1;//设置运行方式，默认为常规窗口
+            shortcut.Description = description;//设置备注
+            shortcut.IconLocation = string.IsNullOrWhiteSpace(iconLocation) ? targetPath : iconLocation;//设置图标路径
+            shortcut.Save();//保存快捷方式
+        }
+
+        /// <summary>
+        /// 创建桌面快捷方式
+        /// </summary>
+        /// <param name="shortcutName">快捷方式名称</param>
+        /// <param name="targetPath">目标路径</param>
+        /// <param name="description">描述</param>
+        /// <param name="iconLocation">图标路径，格式为"可执行文件或DLL路径, 图标编号"</param>
+        /// <remarks></remarks>
+        public static void CreateShortcutOnDesktop(string shortcutName, string targetPath,
+            string description = null, string iconLocation = null)
+        {
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);//获取桌面文件夹路径
+            CreateShortcut(desktop, shortcutName, targetPath, description, iconLocation);
+        }
+    }
+}
