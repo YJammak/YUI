8c5e427674a0258e248851d1590a3038554d5596
Y.J.K <yjk@yjk.name>
2018-05-04 00:13:25 +0800

    更新热键

--- YUI/YUtil/YHotKeyHelper.cs
+++ YUI/YUtil/YHotKeyHelper.cs
@@ -1,229 +1,213 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
+
+#pragma warning disable 1591
 
 namespace YUI.YUtil
 {
     /// <summary>
     /// 自定义按键枚举
     /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public enum YKeys
     {
-        /// <summary>
-        /// 
-        /// </summary>
+        None = 0,
+        LButton = 1,
+        RButton = 2,
+        Cancel = 3,
+        MButton = 4,
+        XButton1 = 5,
+        XButton2 = 6,
+        Back = 8,
+        Tab = 9,
+        LineFeed = 10,
+        Clear = 12,
+        Return = 13,
+        Enter = 13,
+        ShiftKey = 16,
+        ControlKey = 17,
+        Menu = 18,
+        Pause = 19,
+        Capital = 20,
+        CapsLock = 20,
+        KanaMode = 21,
+        HanguelMode = 21,
+        HangulMode = 21,
+        JunjaMode = 23,
+        FinalMode = 24,
+        HanjaMode = 25,
+        KanjiMode = 25,
+        Escape = 27,
+        IMEConvert = 28,
+        IMENonconvert = 29,
+        IMEAccept = 30,
+        IMEAceept = 30,
+        IMEModeChange = 31,
         Space = 32,
-
-        /// <summary>
-        /// 
-        /// </summary>
+        Prior = 33,
+        PageUp = 33,
+        Next = 34,
+        PageDown = 34,
+        End = 35,
+        Home = 36,
         Left = 37,
-
-        /// <summary>
-        /// 
-        /// </summary>
         Up = 38,
-
-        /// <summary>
-        /// 
-        /// </summary>
         Right = 39,
-
-        /// <summary>
-        /// 
-        /// </summary>
         Down = 40,
-
-        /// <summary>
-        /// 
-        /// </summary>
+        Select = 41,
+        Print = 42,
+        Execute = 43,
+        Snapshot = 44,
+        PrintScreen = 44,
+        Insert = 45,
+        Delete = 46,
+        Help = 47,
+        D0 = 48,
+        D1 = 49,
+        D2 = 50,
+        D3 = 51,
+        D4 = 52,
+        D5 = 53,
+        D6 = 54,
+        D7 = 55,
+        D8 = 56,
+        D9 = 57,
         A = 65,
-
-        /// <summary>
-        /// 
-        /// </summary>
         B = 66,
-
-        /// <summary>
-        /// 
-        /// </summary>
         C = 67,
-
-        /// <summary>
-        /// 
-        /// </summary>
         D = 68,
-
-        /// <summary>
-        /// 
-        /// </summary>
         E = 69,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F = 70,
-
-        /// <summary>
-        /// 
-        /// </summary>
         G = 71,
-
-        /// <summary>
-        /// 
-        /// </summary>
         H = 72,
-
-        /// <summary>
-        /// 
-        /// </summary>
         I = 73,
-
-        /// <summary>
-        /// 
-        /// </summary>
         J = 74,
-
-        /// <summary>
-        /// 
-        /// </summary>
         K = 75,
-
-        /// <summary>
-        /// 
-        /// </summary>
         L = 76,
-
-        /// <summary>
-        /// 
-        /// </summary>
         M = 77,
-
-        /// <summary>
-        /// 
-        /// </summary>
         N = 78,
-
-        /// <summary>
-        /// 
-        /// </summary>
         O = 79,
-
-        /// <summary>
-        /// 
-        /// </summary>
         P = 80,
-
-        /// <summary>
-        /// 
-        /// </summary>
         Q = 81,
-
-        /// <summary>
-        /// 
-        /// </summary>
         R = 82,
-
-        /// <summary>
-        /// 
-        /// </summary>
         S = 83,
-
-        /// <summary>
-        /// 
-        /// </summary>
         T = 84,
-
-        /// <summary>
-        /// 
-        /// </summary>
         U = 85,
-
-        /// <summary>
-        /// 
-        /// </summary>
         V = 86,
-
-        /// <summary>
-        /// 
-        /// </summary>
         W = 87,
-
-        /// <summary>
-        /// 
-        /// </summary>
         X = 88,
-
-        /// <summary>
-        /// 
-        /// </summary>
         Y = 89,
-
-        /// <summary>
-        /// 
-        /// </summary>
         Z = 90,
-
-        /// <summary>
-        /// 
-        /// </summary>
+        LWin = 91,
+        RWin = 92,
+        Apps = 93,
+        Sleep = 95,
+        NumPad0 = 96,
+        NumPad1 = 97,
+        NumPad2 = 98,
+        NumPad3 = 99,
+        NumPad4 = 100,
+        NumPad5 = 101,
+        NumPad6 = 102,
+        NumPad7 = 103,
+        NumPad8 = 104,
+        NumPad9 = 105,
+        Multiply = 106,
+        Add = 107,
+        Separator = 108,
+        Subtract = 109,
+        Decimal = 110,
+        Divide = 111,
         F1 = 112,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F2 = 113,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F3 = 114,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F4 = 115,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F5 = 116,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F6 = 117,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F7 = 118,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F8 = 119,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F9 = 120,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F10 = 121,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F11 = 122,
-
-        /// <summary>
-        /// 
-        /// </summary>
         F12 = 123,
+        F13 = 124,
+        F14 = 125,
+        F15 = 126,
+        F16 = 127,
+        F17 = 128,
+        F18 = 129,
+        F19 = 130,
+        F20 = 131,
+        F21 = 132,
+        F22 = 133,
+        F23 = 134,
+        F24 = 135,
+        NumLock = 144,
+        Scroll = 145,
+        LShiftKey = 160,
+        RShiftKey = 161,
+        LControlKey = 162,
+        RControlKey = 163,
+        LMenu = 164,
+        RMenu = 165,
+        BrowserBack = 166,
+        BrowserForward = 167,
+        BrowserRefresh = 168,
+        BrowserStop = 169,
+        BrowserSearch = 170,
+        BrowserFavorites = 171,
+        BrowserHome = 172,
+        VolumeMute = 173,
+        VolumeDown = 174,
+        VolumeUp = 175,
+        MediaNextTrack = 176,
+        MediaPreviousTrack = 177,
+        MediaStop = 178,
+        MediaPlayPause = 179,
+        LaunchMail = 180,
+        SelectMedia = 181,
+        LaunchApplication1 = 182,
+        LaunchApplication2 = 183,
+        OemSemicolon = 186,
+        Oem1 = 186,
+        Oemplus = 187,
+        Oemcomma = 188,
+        OemMinus = 189,
+        OemPeriod = 190,
+        OemQuestion = 191,
+        Oem2 = 191,
+        Oemtilde = 192,
+        Oem3 = 192,
+        OemOpenBrackets = 219,
+        Oem4 = 219,
+        OemPipe = 220,
+        Oem5 = 220,
+        OemCloseBrackets = 221,
+        Oem6 = 221,
+        OemQuotes = 222,
+        Oem7 = 222,
+        Oem8 = 223,
+        OemBackslash = 226,
+        Oem102 = 226,
+        ProcessKey = 229,
+        Packet = 231,
+        Attn = 246,
+        Crsel = 247,
+        Exsel = 248,
+        EraseEof = 249,
+        Play = 250,
+        Zoom = 251,
+        NoName = 252,
+        Pa1 = 253,
+        OemClear = 254,
+        KeyCode = 65535,
+        Shift = 65536,
+        Control = 131072,
+        Alt = 262144,
     }
 
     /// <inheritdoc />
